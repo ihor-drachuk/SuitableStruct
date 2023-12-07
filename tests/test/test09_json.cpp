a85bf0ac8714d5fff4045a78720a302ba6c2f441
Sergiy Proskurnya <sergiy.proskurnya@developex.net>
2023-12-07 19:01:16 +0200

    Added test for integrals

--- tests/test/test09_json.cpp
+++ tests/test/test09_json.cpp
@@ -13,6 +13,7 @@
 #include <QPoint>
 #include <QJsonValue>
 #include <QJsonObject>
+#include <QJsonDocument>
 #include <QDateTime>
 #include <QTimeZone>
 #include <QColor>
@@ -50,8 +51,8 @@ struct Struct1
     bool a0{};
     short a{};
     long b{};
-    QString c;
-    // missing d
+    uint32_t c{};
+    QString d;
     Enum1 e1{};
     Test9_CustomEnum::Enum2 e2{};
     Test9_CustomEnum::Enum2 e3{};
@@ -73,10 +74,72 @@ struct Struct1
     QJsonArray t3;
     QColor u;
 
-    auto ssTuple() const { return std::tie(a0, a, b, c, e1, e2, e3, f, g, h, k, l, m, n, o, p, q, r, s1, s2, t1, t2, t3, u); }
-    auto ssNamesTuple() const { return std::tie("a0", "a", "b", "c", "e1", "e2", "e3", "f", "g", "h", "k", "l", "m", "n", "o", "p", "q", "r", "s1", "s2", "t1", "t2", "t3", "u"); }
+    auto ssTuple() const { return std::tie(a0, a, b, c, d, e1, e2, e3, f, g, h, k, l, m, n, o, p, q, r, s1, s2, t1, t2, t3, u); }
+    auto ssNamesTuple() const { return std::tie("a0", "a", "b", "c", "d", "e1", "e2", "e3", "f", "g", "h", "k", "l", "m", "n", "o", "p", "q", "r", "s1", "s2", "t1", "t2", "t3", "u"); }
     SS_COMPARISONS_MEMBER_ONLY_EQ(Struct1);
 };
+
+struct Struct2
+{
+    int8_t a0{};
+    int8_t a1{};
+
+    uint8_t b0{};
+    uint8_t b1{};
+
+    int16_t c0{};
+    int16_t c1{};
+
+    uint16_t d0{};
+    uint16_t d1{};
+
+    int32_t e0{};
+    int32_t e1{};
+
+    uint32_t f0{};
+    uint32_t f1{};
+
+    int64_t g0{};
+    int64_t g1{};
+
+    uint64_t h0{};
+    uint64_t h1{};
+
+    auto ssTuple() const { return std::tie(a0, a1, b0, b1, c0, c1, d0, d1, e0, e1, f0, f1, g0, g1, h0, h1); }
+    auto ssNamesTuple() const { return std::tie("a0", "a1", "b0", "b1", "c0", "c1", "d0", "d1", "e0", "e1", "f0", "f1", "g0", "g1", "h0", "h1"); }
+};
+
+const QJsonValue testJson = []() {
+  auto configText = "{"
+                    "\"content\": {"
+                      "\"a0\": \"127\","
+                      "\"a1\": \"-0x80\","
+
+                      "\"b0\": \"255\","
+                      "\"b1\": \"0x80\","
+
+                      "\"c0\": \"12345\","
+                      "\"c1\": \"-0x3039\","
+
+                      "\"d0\": \"65535\","
+                      "\"d1\": \"0xFFFF\","
+
+                      "\"e0\": \"2147483647\","
+                      "\"e1\": \"-0x75BCD15\","
+
+                      "\"f0\": \"4294967295\","
+                      "\"f1\": \"0x075BCD15\","
+
+                      "\"g0\": \"-1234567890123456\","
+                      "\"g1\": \"0x7FFFFFFFFFFFFFFF\","
+
+                      "\"h0\": \"123456789012345678\","
+                      "\"h1\": \"0xFFFFFFFFFFFFFFFF\""
+                      "}"
+                    "}";
+
+  return QJsonDocument::fromJson(configText).object();
+}();
 
 } // namespace
 
@@ -89,7 +152,8 @@ TEST(SuitableStruct, JsonSerialization)
     a.a0 = true;
     a.a = 10;
     a.b = 15;
-    a.c = "Test";
+    a.c = 0x6E33168B;
+    a.d = "Test";
     a.e1 = Enum1::Value2;
     a.e2 = Test9_CustomEnum::Enum2::Value3;
     a.e3 = static_cast<Test9_CustomEnum::Enum2>(123123);
@@ -137,6 +201,36 @@ TEST(SuitableStruct, JsonSerialization)
         ssJsonLoad(saved2, c);
         ASSERT_EQ(c, a);
     }
+}
+
+TEST(SuitableStruct, JsonDeserializationIntegrals)
+{
+    Struct2 a;
+    ssJsonLoad(testJson, a, false);
+
+    ASSERT_EQ(a.a0, 127);
+    ASSERT_EQ(a.a1, -0x80);
+
+    ASSERT_EQ(a.b0, 255);
+    ASSERT_EQ(a.b1, 0x80);
+
+    ASSERT_EQ(a.c0, 12345);
+    ASSERT_EQ(a.c1, -0x3039);
+
+    ASSERT_EQ(a.d0, 65535);
+    ASSERT_EQ(a.d1, 0xFFFF);
+
+    ASSERT_EQ(a.e0, 2147483647);
+    ASSERT_EQ(a.e1, -0x75BCD15);
+
+    ASSERT_EQ(a.f0, 4294967295);
+    ASSERT_EQ(a.f1, 0x075BCD15);
+
+    ASSERT_EQ(a.g0, -1234567890123456);
+    ASSERT_EQ(a.g1, 0x7FFFFFFFFFFFFFFF);
+
+    ASSERT_EQ(a.h0, 123456789012345678);
+    ASSERT_EQ(a.h1, 0xFFFFFFFFFFFFFFFF);
 }
 
 TEST(SuitableStruct, JsonSerialization_DateTime)
